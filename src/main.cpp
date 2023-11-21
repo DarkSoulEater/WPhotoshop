09bfc833f9a4175d288bb60a06c0fab85d1dc445
Platonov Timofey <platonov.ta@phystech.edu>
2023-11-22 00:58:57 +0300

    * Add core start

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,17 @@
 #include "cmdline_parser.hpp"
+#include "Core/Core.h"
+
+int MainPhotoshop(Core& core);
 
 int main(int argc, char *argv[]) {
     ParseArgs(argc, argv);
+
+    Core core;
+    return MainPhotoshop(core);
+}
+
+int MainPhotoshop(Core& core) {
+    VertexArray vt(PrimitiveType::Lines, 10);
+    core.Run();
     return 0;
 }
