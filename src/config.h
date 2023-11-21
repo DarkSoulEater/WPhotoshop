2816b9ea1644bb8e4339655df47ef8b79a3cdba7
Platonov Timofey <platonov.ta@phystech.edu>
2023-11-21 22:34:01 +0300

    * Add global config

---
+++ src/config.h
@@ -0,0 +1,21 @@
+#ifndef ___CONFIG_H___
+#define ___CONFIG_H___
+
+#define EMIT_GASE
+
+// ---------- Debug settings ----------
+// Widget
+// #define DDRAW_ONLY_WIDGET_BOUNDS
+
+#ifdef DDRAW_ONLY_WIDGET_BOUNDS
+    #define DDRAW_WIDGET_BOUNDS
+#else
+    #define DDRAW_WIDGET_BOUNDS
+#endif
+
+
+// Pallete
+#define LOG_SWITCH_TOOL
+// ------------------------------------
+
+#endif // ___CONFIG_H___
